0a4d31157027f98056c60f88251026589df95741
Alexey Chicherin <chicherin.alexey@gmail.com>
2022-04-17 10:11:34 +0300

    Fixed widget expanding

--- chatWidget.cpp
+++ chatWidget.cpp
@@ -1,8 +1,46 @@
-#include "chatWidget.h"
-
+﻿#include "chatWidget.h"
 chatWidget::chatWidget(QObject* parent) :QWidget(qobject_cast<QWidget*>(parent))
 {
-	QMessageBox messageBox;
-	messageBox.setText("This is message");
-	messageBox.exec();
+	QGridLayout* layout = new QGridLayout(this);
+	
+	layout->setAlignment(Qt::AlignLeft);
+	layout->setColumnStretch(0, 0);
+	layout->setColumnStretch(1, 1);
+	layout->setColumnStretch(2, 0);
+
+	QLabel* labelLocalPort = new QLabel(this);
+	QLineEdit* editLocalPort = new QLineEdit(this);
+	labelLocalPort->setBuddy(editLocalPort);
+	labelLocalPort->setText(tr("Локальный порт"));
+
+	QLabel* labelRemotePort = new QLabel(this);
+	QLineEdit* editRemotePort = new QLineEdit(this);
+	labelRemotePort->setBuddy(editRemotePort);
+	labelRemotePort->setText(tr("Удалённый порт"));
+
+	QPushButton* btnConnect = new QPushButton(this);
+	btnConnect->setText(tr("Подключиться"));
+
+	QPushButton* btnDisconnect = new QPushButton(this);
+	btnDisconnect->setText(tr("Отключиться"));
+
+	QTextEdit* chatWindow = new QTextEdit(this);
+	QLineEdit* editMessage = new QLineEdit(this);
+	QPushButton* btnSend = new QPushButton(this);
+	btnSend->setText(tr("Отправить"));
+
+	layout->addWidget(labelLocalPort,  0, 0, Qt::AlignLeft);
+	layout->addWidget(editLocalPort,   0, 1);
+	layout->addWidget(btnConnect,      0, 2);
+
+	layout->addWidget(labelRemotePort, 1, 0, Qt::AlignLeft);
+	layout->addWidget(editRemotePort,  1, 1);
+	layout->addWidget(btnDisconnect,   1, 2);
+
+	layout->addWidget(chatWindow,      2, 0, 1, 3);
+
+	layout->addWidget(editMessage,     3, 0,1,2);
+	layout->addWidget(btnSend,         3, 2);
+
+	setLayout(layout);
 }
--- chatWidget.h
+++ chatWidget.h
@@ -1,5 +1,15 @@
 #include <QWidget>
 #include <QMessageBox>
+#include <QGridLayout>
+#include <QLineEdit>
+#include <QTextEdit>
+#include <QPushButton>
+#include <QLabel>
+#include <QScreen>
+#include <QWindow>
+#include <QDebug>
+#include <QSizeGrip>
+
 class chatWidget : public QWidget
 {
 public:
